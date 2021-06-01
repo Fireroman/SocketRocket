c4b32193aa6070952078e220e66ac79b6daf3d35
Roman <85091414+Fireroman@users.noreply.github.com>
2021-06-01 22:00:52 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
       .iOS(.v10)
    ],
    products: [
-      .library(name: "SocketRocket", targets: ["SocketRocket", "~> 0.5.1"])
+      .library(name: "SocketRocket", targets: ["SocketRocket"])
    ],
    targets: [
       .target(
